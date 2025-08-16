5c20921d5a1f3a7dc6fa7964c4d206cc7f5e56d2
Xavier Chen <xaviernehc@gmail.com>
2025-08-16 19:35:43 +0800

    feat: change to only have top navbar regardless of scroll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,21 +1,11 @@
 import React, { useEffect, useRef, useState } from "react";
 import { motion } from "motion/react";
 
+// TODO: change sidebar to top bar
 export function NavBar({ items, className }) {
   const [activeTab, setActiveTab] = useState(items[0].name);
-  const [isScrolled, setIsScrolled] = useState(false);
   const isManualNavigate = useRef(false);
   const clickTimeoutRef = useRef(null);
-
-  // Add Event listener to detect scroll when it goes past viewport
-  useEffect(() => {
-    const handleScroll = () => {
-      // setIsScrolled(window.scrollY > window.innerHeight);
-      setIsScrolled(window.scrollY > 500);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
 
   // Intersection Observer to detect active sessions
   useEffect(() => {
@@ -85,16 +75,10 @@ export function NavBar({ items, className }) {
   // 3. create each item's button
   return (
     <div
-      className={`fixed bottom-0 left-1/2 z-50 mb-6 -translate-x-1/2 transition-all duration-500 ease-out md:top-0 md:mt-6 ${
-        isScrolled
-          ? "ml-9 md:top-1/2 md:left-6 md:-translate-y-1/2 md:flex-col"
-          : "md:top-0 md:mt-6"
-      }`}
+      className={`fixed bottom-0 left-1/2 z-50 mb-6 -translate-x-1/2 transition-all duration-500 ease-out md:top-0 md:mt-6`}
     >
       <div
-        className={`flex items-center gap-3 rounded-full bg-gray-100/5 px-3 py-1 shadow-lg backdrop-blur-lg transition-all duration-300 ease-out ${
-          isScrolled ? "md:flex-col md:gap-3 md:px-1 md:py-3" : ""
-        }`}
+        className={`flex items-center gap-3 rounded-full bg-gray-100/5 px-3 py-1 shadow-lg backdrop-blur-lg transition-all duration-300 ease-out`}
       >
         {items.map((item) => {
           const Icon = item.icon;
@@ -109,10 +93,8 @@ export function NavBar({ items, className }) {
                 isActive ? "bg-rosepinehighlight-high" : ""
               }`}
             >
-              <span className={`hidden ${isScrolled ? "" : "md:inline"} `}>
-                {item.name}
-              </span>
-              <span className={`${isScrolled ? "" : "md:hidden"}`}>
+              <span className="hidden inline">{item.name}</span>
+              <span className="md:hidden">
                 <Icon size={18} strokeWidth={2.5} />
               </span>
               {isActive && (
@@ -126,28 +108,10 @@ export function NavBar({ items, className }) {
                     duration: 0.1,
                   }}
                 >
-                  <div
-                    className={`bg-rosepinelove absolute -top-2 left-1/2 h-1 w-4 -translate-x-1/2 rounded-t-full ${
-                      isScrolled
-                        ? "md:top-1/2 md:-left-2 md:h-4 md:w-1 md:-translate-y-1/2 md:rounded-full"
-                        : ""
-                    }`}
-                  >
-                    <div
-                      className={`bg-rosepinelove/20 absolute -top-2 -left-2 h-6 w-8 rounded-full blur-md ${
-                        isScrolled ? "md:h-8 md:w-6" : ""
-                      }`}
-                    >
-                      <div
-                        className={`bg-rosepinelove/20 absolute -top-1 h-6 w-8 rounded-full blur-md ${
-                          isScrolled ? "md:-left-1 md:h-8 md:w-6" : ""
-                        }`}
-                      >
-                        <div
-                          className={`bg-rosepinelove/20 absolute top-0 h-4 w-4 rounded-full ${
-                            isScrolled ? "md:left-0" : ""
-                          }`}
-                        ></div>
+                  <div className="bg-rosepinelove absolute -top-2 left-1/2 h-1 w-4 -translate-x-1/2 rounded-t-full">
+                    <div className="bg-rosepinelove/20 absolute -top-2 -left-2 h-6 w-8 rounded-full blur-md">
+                      <div className="bg-rosepinelove/20 absolute -top-1 h-6 w-8 rounded-full blur-md">
+                        <div className="bg-rosepinelove/20 absolute top-0 h-4 w-4 rounded-full"></div>
                       </div>
                     </div>
                   </div>
